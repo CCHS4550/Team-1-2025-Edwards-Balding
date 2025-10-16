be977217f6a9aa9032ca89f1a7f8ca0e9d5e39f5
CircularBlox <mathguy12321@gmail.com>
2025-10-16 16:43:33 -0600

    drivetrain work #2
    forgot to commit this last night

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -13,4 +16,16 @@ public class DriveTrain{
     private MotorController frontRightMotor = new MotorController("FrontRightMotor", Constants.OperatorConstants.FRdeviceID, MotorType.kBrushless, false, IdleMode.kBrake);
     private MotorController backLeftMotor = new MotorController("BackLeftMotor", Constants.OperatorConstants.BLdeviceID, MotorType.kBrushless, false, IdleMode.kBrake);
     private MotorController backRightMotor = new MotorController("BackRightMotor", Constants.OperatorConstants.BRdeviceID, MotorType.kBrushless, false, IdleMode.kBrake);
+
+    //Set output to each motor
+    public DifferentialDrive diffDrive = new DifferentialDrive(
+        (double output) -> {
+            frontLeftMotor.set(output);
+            backLeftMotor.set(output);
+        },
+        (double output) -> {
+            frontRightMotor.set(output);
+            backRightMotor.set(output);
+        }
+    );
 }
