e89c1fe656009a8d21507fc339a2295565efe16c
TerryZhang12 <greninja7144@gmail.com>
2025-10-30 16:30:48 -0600

    finished intake???

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,5 +7,9 @@ public final class Constants{
         public static final int FRdeviceID = 2;
         public static final int BLdeviceID = 3;
         public static final int BRdeviceID = 4;
+
+        public static final int flywheelMotorID = 61;
+        public static final double flywheelVoltageDefault = 12;
+        public static final double flywheelSpeedDefault = 0.5;
     }
 }
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,45 @@
 package frc.robot.subsystems.intake;
 
-public class Intake{
-    
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase{
+    public IntakeIO io;
+
+    public Intake(IntakeIO io)
+    {
+        this.io = io;
+    }
+
+    public void setFlywheelVoltage(double voltage)
+    {
+        io.setFlywheelVoltage(voltage);
+    }
+
+    public void setFlywheelSpeed(double speed)
+    {
+        io.setFlywheelSpeed(speed);
+    }
+
+    public void setFlywheelVoltageDefault()
+    {
+        io.setFlywheelVoltageDefault();
+    }
+
+    public void setFlywheelSpeedDefault()
+    {
+        io.setFlywheelSpeedDefault();
+    }
+
+    public Command run()
+    {
+        return this.runEnd(()->setFlywheelSpeedDefault(), (()->setFlywheelVoltage(0)));
+    }
+
+    public Command halt()
+    {
+        return Commands.runOnce(()-> {}, this);
+    }
 }
