764fc9c42692074864102b7f622f9c314cc0c0ae
CircularBlox <mathguy12321@gmail.com>
2025-11-08 12:46:53 -0700

    using wpilibs motorcontroller
    terry was right lol
    
    anyways added in the extra functions and built-in ones
    also uses clamping for setvoltage

--- src/main/java/frc/helpers/MotorController.java
+++ src/main/java/frc/helpers/MotorController.java
@@ -25,170 +25,114 @@ import com.revrobotics.spark.SparkBase;
      * @param positionFactor The ratio of encoder units to desired units (ie. units -> in)
 */
 
-public class MotorController{
-    private SparkMax motor;
-    public SparkClosedLoopController pidController; //Keeping it PID for easy reference
-    private RelativeEncoder encoder;
-    private String name;
+public class MotorController implements edu.wpi.first.wpilibj.motorcontrol.MotorController{
+    private final SparkMax motor;
+    public final SparkClosedLoopController pidController;
+    private final RelativeEncoder encoder;
+    private final String name;
     private double voltageConversionFactor;
 
-    //Configurations to change values for the encoder and motor
-    private EncoderConfig encoderConfig;
-    private SparkMaxConfig sparkMaxConfig;
-    
-    public MotorController(String name, int deviceID, MotorType controlMode, boolean inverted, IdleMode idleMode, double positionFactor){
-        this.motor = new SparkMax(deviceID, controlMode);
-        this.name = name;
-        this.pidController = motor.getClosedLoopController();
-        this.encoderConfig = new EncoderConfig();
-        this.sparkMaxConfig = new SparkMaxConfig();
+    private final EncoderConfig encoderConfig;
+    private final SparkMaxConfig sparkMaxConfig;
 
+    public MotorControllerWrapper(String name, int deviceID, MotorType motorType, boolean inverted, IdleMode idleMode, double positionFactor){
+        this.name = name;
+        motor = new SparkMax(deviceID, motorType);
+        pidController = motor.getClosedLoopController();
+        voltageConversionFactor = Constants.OperatorConstants.voltageConversionFactor;
+
+        //Encoder config
+        encoderConfig = new EncoderConfig();
         encoderConfig.positionConversionFactor(positionFactor);
 
+        //SparkMax config
+        sparkMaxConfig = new SparkMaxConfig();
         sparkMaxConfig.inverted(inverted);
         sparkMaxConfig.idleMode(idleMode);
         sparkMaxConfig.apply(encoderConfig);
 
         motor.configure(sparkMaxConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
 
-        this.encoder = motor.getEncoder();
-        this.voltageConversionFactor = Constants.OperatorConstants.voltageConversionFactor;
-    }
-    public MotorController(String name, int deviceID, MotorType controlMode, boolean inverted, IdleMode idleMode){
-        this(name, deviceID, controlMode, inverted, idleMode, 1.0);
+        encoder = motor.getEncoder();
     }
 
-    public void reset(){
-        encoder.setPosition(0);
+    public MotorControllerWrapper(String name, int deviceID, MotorType motorType, boolean inverted, IdleMode idleMode){
+        this(name, deviceID, motorType, inverted, idleMode, 1.0);
     }
 
+    //Built-in
+    @Override
     public void set(double speed){
         motor.set(speed);
     }
 
-    public void set(boolean stop, double speed){
-        if(!stop){
-            motor.set(speed);
-        }else{
-            motor.set(0);
-        }
-    }
-
-    //From -1 to 1, negatives are backwards
-    public double getOutput(){
+    @Override
+    public double get(){
         return motor.get();
     }
 
-    public void disable(){
-        motor.disable();
+    @Override
+    public void stopMotor(){
+        motor.set(0);
     }
 
-    public void setVoltage(){
-        //motor.setVoltage(Constants.OperatorConstants.ShooterVoltage);
-        motor.setVoltage(12.0);
+    @Override
+    public void setInverted(boolean isInverted){
+        motor.setInverted(isInverted);
     }
 
-    public void setVoltage(double volts){
-        motor.setVoltage(volts);
+    @Override
+    public boolean getInverted(){
+        return motor.getInverted();
     }
 
-    public void setVoltage(double volts, double limit){
-        if(motor.getOutputCurrent() <= limit){
-            motor.setVoltage(volts);
-        }else{
-            motor.setVoltage(0);
-        }
+    //SparkMax specific
+    public void resetEncoder(){
+        encoder.setPosition(0);
     }
 
-    public void setVoltageFromSpeed(double speed){
-        motor.setVoltage(speed*voltageConversionFactor);
+    public double getPosition(){
+        return encoder.getPosition();
     }
 
-    public double getVoltage(){
-        return motor.getAppliedOutput() * 12.0;
+    public void setPosition(double pos){
+        encoder.setPosition(pos);
     }
-
-    //Electric current in amps that motor is drawing
-    public double getCurrent(){
-        return motor.getOutputCurrent();
-    }
-
 
     public double getVelocity(){
         return encoder.getVelocity();
     }
 
-    public void setPositionConversionFactor(double factor){
-        encoderConfig.positionConversionFactor(factor);
-        sparkMaxConfig.apply(encoderConfig);
-        motor.configure(sparkMaxConfig,SparkBase.ResetMode.kResetSafeParameters,SparkBase.PersistMode.kNoPersistParameters);
-    }
-
-    public void setVelocityConversionFactor(double factor) {
-        encoderConfig.velocityConversionFactor(factor);
-        sparkMaxConfig.apply(encoderConfig);
-        motor.configure(sparkMaxConfig,SparkBase.ResetMode.kResetSafeParameters,SparkBase.PersistMode.kNoPersistParameters);
-    }
-
-    //Sets the encoder position
-    public void setPosition(double pos){
-        encoder.setPosition(pos);
-    }
-
-    //By default the position is in encoder units, but will return a distance if the Position Conversion Factor has been set.
-    public double getPosition(){
-        return encoder.getPosition();
-    }
-    
-    /**
-     * Sets the PID values, must be positive
-     * @param Kp The proportional gain value
-     * @param Ki The integral gain value
-     * @param Kd The derivative gain value
-     */
-    public void setPID(double Kp, double Ki, double Kd, double ff){
-        ClosedLoopConfig closedLoopConfig = new ClosedLoopConfig();
-        closedLoopConfig.pidf(Kp,Ki,Kd,ff); //Going to be deprecated but too lazy to fix it up right now
-
-        sparkMaxConfig.apply(closedLoopConfig);
-        motor.configure(sparkMaxConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+    public void setPID(double kP, double kI, double kD, double kFF){
+        pidController.setPIDF(kP, kI, kD, kFF);
     }
 
     public void setReferencePosition(double pos){
         pidController.setReference(pos, ControlType.kPosition);
     }
-    
+
+    public void setVoltage(double volts){
+        double capped = MathUtil.clamp(volts, -12.0, 12.0);
+        motor.setVoltage(capped);
+    }
+
+    public void setVoltageFromSpeed(double speed){
+        motor.setVoltage(speed * voltageConversionFactor);
+    }
+
+    public void setPositionConversionFactor(double factor){
+        encoderConfig.positionConversionFactor(factor);
+        sparkMaxConfig.apply(encoderConfig);
+        motor.configure(sparkMaxConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+    }
+
+    public void setVelocityConversionFactor(double factor){
+        encoderConfig.velocityConversionFactor(factor);
+        sparkMaxConfig.apply(encoderConfig);
+        motor.configure(sparkMaxConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+    }
+
     public String getName(){
         return name;
-    }
-
-    public void setInverted(boolean isInverted){
-        sparkMaxConfig.inverted(isInverted);
-        motor.configure(sparkMaxConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
-    }
-
-    public boolean getInverted(){
-        return motor.getInverted();
-    }
-
-    //Some other functions
-    //Permanently saves configuration to Spark MAX flash memory
-    public void burnFlash(){
-        motor.burnFlash();
-    }
-
-    //Restore factory defaults (resetting weird configs usually)
-    public void restoreFactoryDefaults() {
-        motor.restoreFactoryDefaults();
-    }
-
-    //Set a new idle mode (Brake or Coast)
-    public void setIdleMode(IdleMode mode) {
-        sparkMaxConfig.idleMode(mode);
-        motor.configure(sparkMaxConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
-    }
-
-    public void stopMotor(){
-        motor.set(0);
     }
 }
